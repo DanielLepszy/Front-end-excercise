e52b50adc836c639507c5b353a02ab62a84f3878
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-07 11:31:15 +0200

    fix the slider options

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,37 +1,38 @@
-const getWeekFromPage = () => {
-    return document.getElementsByClassName("getWeek")[0].innerHTML[5];
+const sliderContent = ["Week 1", "Week 2", "Week 3", "Week 4"]
+
+const getContentFromPage = () => {
+    return document.getElementsByClassName("getWeek")[0].innerHTML;
 }
 
 const incrementNumberOfWeek = (x) => {
-    var currentNumberOfWeek = getWeekFromPage();
-    document.getElementsByClassName("arrows")[0].style.display = 'block'
-    if (currentNumberOfWeek >= 4) {
-        x.style.display = 'none';
-        changeNumberOfDays()
-    } else {
-        currentNumberOfWeek++
-        document.getElementsByClassName("getWeek")[0].innerHTML = "Week " + currentNumberOfWeek
-        changeNumberOfDays()
+    var currentNumberOfWeek = getContentFromPage();
+
+    if (currentNumberOfWeek !== sliderContent[(sliderContent.length - 1)]) {
+        document.getElementsByClassName("arrows")[0].style.opacity = "1";
+        var i = 0;
+        while (currentNumberOfWeek !== sliderContent[i]) {
+            i++
+        }
+
+        document.getElementsByClassName("getWeek")[0].innerHTML = sliderContent[i + 1]
+        if (i === sliderContent.length - 2) {
+            x.style.opacity = "0";
+        }
     }
+
 }
 
 const decrementNumberOfWeek = (x) => {
-    var currentNumberOfWeek = getWeekFromPage();
-    document.getElementsByClassName("arrows")[1].style.display = 'block'
-    if (currentNumberOfWeek <= 1) {
-        x.style.display = 'none';
-        changeNumberOfDays()
-    } else {
-        currentNumberOfWeek--
-        document.getElementsByClassName("getWeek")[0].innerHTML = "Week " + currentNumberOfWeek
-        changeNumberOfDays()
-    }
-}
-
-const changeNumberOfDays = () => {
-    var currentNumberOfWeek = getWeekFromPage();
-    var weekDay = document.getElementsByTagName("th");
-    for (var i = 7; i >= 1; i--) {
-        weekDay[i].innerHTML = "DAY " + ((currentNumberOfWeek * 7) - (-i + 7))
+    var currentNumberOfWeek = getContentFromPage();
+    if (currentNumberOfWeek !== sliderContent[0]) {
+        document.getElementsByClassName("arrows")[1].style.opacity = "1";
+        var i = 0;
+        while (currentNumberOfWeek !== sliderContent[i]) {
+            i++
+        }
+        document.getElementsByClassName("getWeek")[0].innerHTML = sliderContent[i - 1]
+        if (i === 1 ){
+            x.style.opacity = "0";
+        }
     }
 }
