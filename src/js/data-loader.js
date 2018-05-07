00c0370c1f5399d102e7dc1b4b117f68c7d0cbd4
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-07 22:14:30 +0200

    change url of non-images and divide functions

--- src/js/data-loader.js
+++ src/js/data-loader.js
@@ -44,27 +44,85 @@ const setWeekProgressImages = () => {
     numberOfWeeks[11].innerText = 12;
 
 }
-const setHoursOnTheTable = () => {
+const setHoursAndDaysOnTheTable = () => {
     const getProperRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr');
-  getProperRow[1].getElementsByTagName('td')[0].innerHTML  = data.hours[6].hour;
-  getProperRow[2].getElementsByTagName('td')[0].innerHTML  = data.hours[9].hour;
-  getProperRow[3].getElementsByTagName('td')[0].innerHTML  = data.hours[12].hour;
-  getProperRow[4].getElementsByTagName('td')[0].innerHTML  = data.hours[15].hour;
-  getProperRow[5].getElementsByTagName('td')[0].innerHTML  = data.hours[18].hour;
 
+    getProperRow[1].getElementsByTagName('td')[0].innerHTML = data.hours[6].hour;
+    getProperRow[2].getElementsByTagName('td')[0].innerHTML = data.hours[9].hour;
+    getProperRow[3].getElementsByTagName('td')[0].innerHTML = data.hours[12].hour;
+    getProperRow[4].getElementsByTagName('td')[0].innerHTML = data.hours[15].hour;
+    getProperRow[5].getElementsByTagName('td')[0].innerHTML = data.hours[18].hour;
+
+    getProperRow[0].getElementsByTagName('th')[1].innerHTML = data.weeks[0].Day[0].NumberOfDay
+    getProperRow[0].getElementsByTagName('th')[2].innerHTML = data.weeks[0].Day[1].NumberOfDay
+    getProperRow[0].getElementsByTagName('th')[3].innerHTML = data.weeks[0].Day[2].NumberOfDay
+    getProperRow[0].getElementsByTagName('th')[4].innerHTML = data.weeks[0].Day[3].NumberOfDay
+    getProperRow[0].getElementsByTagName('th')[5].innerHTML = data.weeks[0].Day[4].NumberOfDay
+    getProperRow[0].getElementsByTagName('th')[6].innerHTML = data.weeks[0].Day[5].NumberOfDay
+    getProperRow[0].getElementsByTagName('th')[7].innerHTML = data.weeks[0].Day[6].NumberOfDay
 }
-const setDishesToTheTable = () => {
+const setBreakfastToTheTable = () => {
     const breakfastRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr')[1].getElementsByTagName('td')
+    breakfastRow[1].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[0].Dishes[0].breakfast
+    breakfastRow[2].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[1].Dishes[0].breakfast
+    breakfastRow[3].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[2].Dishes[0].breakfast
+    breakfastRow[4].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[3].Dishes[0].breakfast
+    breakfastRow[5].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[4].Dishes[0].breakfast
+    breakfastRow[6].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[5].Dishes[0].breakfast
+   // breakfastRow[2].getElementsByTagName('img')[0].style="filter: grayscale(100%)";   
+}
+const setSecondBreakfastToTheTable = () => {
     const secondBreakfastRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr')[2].getElementsByTagName('td')
+    secondBreakfastRow[1].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[0].Dishes[1].secondBreakfast
+    secondBreakfastRow[2].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[1].Dishes[1].secondBreakfast
+    secondBreakfastRow[3].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[2].Dishes[1].secondBreakfast
+    secondBreakfastRow[4].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[3].Dishes[1].secondBreakfast
+    secondBreakfastRow[5].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[4].Dishes[1].secondBreakfast
+    secondBreakfastRow[6].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[5].Dishes[1].secondBreakfast
+}
+const setDinnerToTheTable = () => {
     const dinnerRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr')[3].getElementsByTagName('td')
+    dinnerRow[1].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[0].Dishes[2].dinner
+    dinnerRow[2].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[1].Dishes[2].dinner
+    dinnerRow[3].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[2].Dishes[2].dinner
+    dinnerRow[4].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[3].Dishes[2].dinner
+    dinnerRow[5].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[4].Dishes[2].dinner
+    dinnerRow[6].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[5].Dishes[2].dinner
+}
+const setDessertToTheTable = () => {
     const dessertRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr')[4].getElementsByTagName('td')
+    dessertRow[1].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[0].Dishes[3].dessert
+    dessertRow[2].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[1].Dishes[3].dessert
+    dessertRow[3].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[2].Dishes[3].dessert
+    dessertRow[4].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[3].Dishes[3].dessert
+    dessertRow[5].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[4].Dishes[3].dessert
+    dessertRow[6].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[5].Dishes[3].dessert
+}
+const setSupperToTheTable = () => {
     const supperRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr')[5].getElementsByTagName('td')
-
-    breakfastRow[1].innerHTML=data.weeks[0].day[0].dishes.breakfast
+    supperRow[1].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[0].Dishes[4].supper
+    supperRow[2].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[1].Dishes[4].supper
+    supperRow[3].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[2].Dishes[4].supper
+    supperRow[4].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[3].Dishes[4].supper
+    supperRow[5].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[4].Dishes[4].supper
+    supperRow[6].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[5].Dishes[4].supper
+}    
+const setLevelOfCarbs = () => {
+    const carbsRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr')[6].getElementsByTagName('td')
+    carbsRow[1].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[0].Dishes[5].carbs
+    carbsRow[2].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[1].Dishes[5].carbs
+    carbsRow[3].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[2].Dishes[5].carbs
+    carbsRow[4].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[3].Dishes[5].carbs
+    carbsRow[5].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[4].Dishes[5].carbs
+    carbsRow[6].getElementsByTagName('p')[0].innerText = data.weeks[0].Day[5].Dishes[5].carbs
 }
 const renderPage = () => {
     setProteinImages();
     setWeekProgressImages();
-    setHoursOnTheTable();
-    
+    setHoursAndDaysOnTheTable();
+    setBreakfastToTheTable();
+    setSecondBreakfastToTheTable();
+    setDinnerToTheTable();
+    setDessertToTheTable();
+    setSupperToTheTable();
 }
