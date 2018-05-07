05298ae030f9eaf04122a2e675c71daf62c2f88d
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-07 15:50:16 +0200

    render images on page

--- src/js/data-loader.js
+++ src/js/data-loader.js
@@ -1,8 +1,31 @@
 const loadJSON = () => {
     var value = $.ajax({
-        url: 'https://raw.githubusercontent.com/DanielLepszy/Best-Employee/master/questions_and_answers.json',
+        url: 'https://rawgit.com/DanielLepszy/Front-end-excercise/work/table-data.json',
         async: false // 
     }).responseText;
     return JSON.parse(value);
 }
 const data = loadJSON();
+const setProteinImages = () =>
+{
+    const proteinImages = document.getElementsByClassName("proteinOptions")[0].getElementsByTagName('img')
+    proteinImages[0].src =data.selectedProtein[0].src;
+    proteinImages[1].src =data.selectedProtein[1].src;
+    proteinImages[2].src =data.selectedProtein[2].src;
+    proteinImages[3].src =data.selectedProtein[8].src;
+    proteinImages[4].src =data.selectedProtein[9].src;
+}
+const setWeekProgressImages = () =>
+{
+    const weekProgressImages = document.getElementsByClassName("weekProgressChart")[0].getElementsByTagName('img')
+    weekProgressImages[1].src =data.weekProgress[1].src;
+    weekProgressImages[1].src =data.weekProgress[1].src;
+    weekProgressImages[2].src =data.weekProgress[2].src;
+    weekProgressImages[3].src =data.weekProgress[1].src;
+    weekProgressImages[4].src =data.weekProgress[2].src;
+}
+
+const renderPage = () =>{
+    setProteinImages();
+    setWeekProgressImages();
+}
