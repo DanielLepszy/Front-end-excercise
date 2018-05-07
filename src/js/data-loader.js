11682dcd1f90fa52f34fe6095a7e61402f7f7cc7
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-07 17:59:24 +0200

    render hours in table

--- src/js/data-loader.js
+++ src/js/data-loader.js
@@ -6,31 +6,29 @@ const loadJSON = () => {
     return JSON.parse(value);
 }
 const data = loadJSON();
-const setProteinImages = () =>
-{
+const setProteinImages = () => {
     const proteinImages = document.getElementsByClassName("proteinOptions")[0].getElementsByTagName('img')
-    proteinImages[0].src =data.selectedProtein[0].src;
-    proteinImages[1].src =data.selectedProtein[1].src;
-    proteinImages[2].src =data.selectedProtein[2].src;
-    proteinImages[3].src =data.selectedProtein[8].src;
-    proteinImages[4].src =data.selectedProtein[9].src;
+    proteinImages[0].src = data.selectedProtein[0].src;
+    proteinImages[1].src = data.selectedProtein[1].src;
+    proteinImages[2].src = data.selectedProtein[2].src;
+    proteinImages[3].src = data.selectedProtein[8].src;
+    proteinImages[4].src = data.selectedProtein[9].src;
 }
-const setWeekProgressImages = () =>
-{
+const setWeekProgressImages = () => {
     const weekProgressImages = document.getElementsByClassName("weekProgressChart")[0].getElementsByTagName('img')
     const numberOfWeeks = document.getElementsByClassName("weekProgressChart")[0].getElementsByTagName('p');
-    weekProgressImages[0].src =data.weekProgress[0].src;
-    weekProgressImages[1].src =data.weekProgress[0].src;
-    weekProgressImages[2].src =data.weekProgress[0].src;
-    weekProgressImages[3].src =data.weekProgress[0].src;
-    weekProgressImages[4].src =data.weekProgress[0].src;
-    weekProgressImages[5].src =data.weekProgress[0].src;
-    weekProgressImages[6].src =data.weekProgress[0].src;
-    weekProgressImages[7].src =data.weekProgress[1].src;
-    weekProgressImages[8].src =data.weekProgress[2].src;
-    weekProgressImages[9].src =data.weekProgress[2].src;
-    weekProgressImages[10].src =data.weekProgress[2].src;
-    weekProgressImages[11].src =data.weekProgress[2].src;
+    weekProgressImages[0].src = data.weekProgress[0].src;
+    weekProgressImages[1].src = data.weekProgress[0].src;
+    weekProgressImages[2].src = data.weekProgress[0].src;
+    weekProgressImages[3].src = data.weekProgress[0].src;
+    weekProgressImages[4].src = data.weekProgress[0].src;
+    weekProgressImages[5].src = data.weekProgress[0].src;
+    weekProgressImages[6].src = data.weekProgress[0].src;
+    weekProgressImages[7].src = data.weekProgress[1].src;
+    weekProgressImages[8].src = data.weekProgress[2].src;
+    weekProgressImages[9].src = data.weekProgress[2].src;
+    weekProgressImages[10].src = data.weekProgress[2].src;
+    weekProgressImages[11].src = data.weekProgress[2].src;
 
     numberOfWeeks[0].innerText = 1;
     numberOfWeeks[1].innerText = 2;
@@ -45,13 +43,21 @@ const setWeekProgressImages = () =>
     numberOfWeeks[10].innerText = 11;
     numberOfWeeks[11].innerText = 12;
 
+}
+const setHoursOnTheTable = () => {
+    const getProperRow = document.getElementsByClassName("tableCells")[0].getElementsByTagName('tr');
+  getProperRow[1].getElementsByTagName('td')[0].innerHTML  = data.hours[6];
+  getProperRow[2].getElementsByTagName('td')[0].innerHTML  = data.hours[9];
+  getProperRow[3].getElementsByTagName('td')[0].innerHTML  = data.hours[12];
+  getProperRow[4].getElementsByTagName('td')[0].innerHTML  = data.hours[15];
+  getProperRow[5].getElementsByTagName('td')[0].innerHTML  = data.hours[18];
 
 }
-const setDishesToTheTable = () =>
-{
+const setDishesToTheTable = () => {
 
 }
-const renderPage = () =>{
+const renderPage = () => {
     setProteinImages();
     setWeekProgressImages();
+    setHoursOnTheTable();
 }
