9fd755de83d760a89cc733dc24ed0376f497bad4
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-07 15:06:05 +0200

    Add ajax function to get JSON from the server

---
+++ src/js/data-loader.js
@@ -0,0 +1,8 @@
+const loadJSON = () => {
+    var value = $.ajax({
+        url: 'https://raw.githubusercontent.com/DanielLepszy/Best-Employee/master/questions_and_answers.json',
+        async: false // 
+    }).responseText;
+    return JSON.parse(value);
+}
+const data = loadJSON();
