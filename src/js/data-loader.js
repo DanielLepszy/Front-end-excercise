417882476038f4c02855f08d0a789acb5c052ed8
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-07 17:00:13 +0200

    add dishes to JSON, working on hover styles

--- src/js/data-loader.js
+++ src/js/data-loader.js
@@ -1,6 +1,6 @@
 const loadJSON = () => {
     var value = $.ajax({
-        url: 'https://rawgit.com/DanielLepszy/Front-end-excercise/work/table-data.json',
+        url: 'https://cdn.rawgit.com/DanielLepszy/Front-end-excercise/work/table-data.json',
         async: false // 
     }).responseText;
     return JSON.parse(value);
@@ -18,13 +18,39 @@ const setProteinImages = () =>
 const setWeekProgressImages = () =>
 {
     const weekProgressImages = document.getElementsByClassName("weekProgressChart")[0].getElementsByTagName('img')
-    weekProgressImages[1].src =data.weekProgress[1].src;
-    weekProgressImages[1].src =data.weekProgress[1].src;
-    weekProgressImages[2].src =data.weekProgress[2].src;
-    weekProgressImages[3].src =data.weekProgress[1].src;
-    weekProgressImages[4].src =data.weekProgress[2].src;
-}
+    const numberOfWeeks = document.getElementsByClassName("weekProgressChart")[0].getElementsByTagName('p');
+    weekProgressImages[0].src =data.weekProgress[0].src;
+    weekProgressImages[1].src =data.weekProgress[0].src;
+    weekProgressImages[2].src =data.weekProgress[0].src;
+    weekProgressImages[3].src =data.weekProgress[0].src;
+    weekProgressImages[4].src =data.weekProgress[0].src;
+    weekProgressImages[5].src =data.weekProgress[0].src;
+    weekProgressImages[6].src =data.weekProgress[0].src;
+    weekProgressImages[7].src =data.weekProgress[1].src;
+    weekProgressImages[8].src =data.weekProgress[2].src;
+    weekProgressImages[9].src =data.weekProgress[2].src;
+    weekProgressImages[10].src =data.weekProgress[2].src;
+    weekProgressImages[11].src =data.weekProgress[2].src;
 
+    numberOfWeeks[0].innerText = 1;
+    numberOfWeeks[1].innerText = 2;
+    numberOfWeeks[2].innerText = 3;
+    numberOfWeeks[3].innerText = 4;
+    numberOfWeeks[4].innerText = 5;
+    numberOfWeeks[5].innerText = 6;
+    numberOfWeeks[6].innerText = 7;
+    numberOfWeeks[7].innerText = 8;
+    numberOfWeeks[8].innerText = 9;
+    numberOfWeeks[9].innerText = 10;
+    numberOfWeeks[10].innerText = 11;
+    numberOfWeeks[11].innerText = 12;
+
+
+}
+const setDishesToTheTable = () =>
+{
+
+}
 const renderPage = () =>{
     setProteinImages();
     setWeekProgressImages();
