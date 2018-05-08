3f3f998c62a6e15fd739fe8805972627f9ed3b2b
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-05-08 09:09:30 +0200

    Add new url server

--- src/js/data-loader.js
+++ src/js/data-loader.js
@@ -1,6 +1,6 @@
 const loadJSON = () => {
     var value = $.ajax({
-        url: 'https://raw.githack.com/DanielLepszy/Front-end-excercise/work/table-data.json',
+        url: 'https://rawgit.com/DanielLepszy/Front-end-excercise/work/table-data.json',
         async: false // 
     }).responseText;
     return JSON.parse(value);
